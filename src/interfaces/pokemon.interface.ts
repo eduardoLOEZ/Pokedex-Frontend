e016b1801915f276a687db089095d55a48e7a658
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2024-05-28 09:05:19 -0600

    comentarios agregados

--- src/interfaces/pokemon.interface.ts
+++ src/interfaces/pokemon.interface.ts
@@ -1,15 +1,24 @@
+/**
+ * Interface para representar una habilidad de un Pokémon.
+ */
 export interface Ability {
-  name: string;
+  name: string; // Nombre de la habilidad
 }
 
+/**
+ * Interface para representar un Pokémon básico.
+ */
 export interface Pokemon {
-  name: string;
-  image: string;
-  abilities: Ability[];
+  name: string; // Nombre del Pokémon
+  image: string; // URL de la imagen del Pokémon
+  abilities: Ability[]; // Lista de habilidades del Pokémon
 }
 
+/**
+ * Interface extendida para representar un Pokémon con detalles adicionales.
+ */
 export interface DetailedPokemon extends Pokemon {
-  base_experience: number;
-  height: number;
-  weight: number;
+  base_experience: number; // Experiencia base del Pokémon
+  height: number; // Altura del Pokémon
+  weight: number; // Peso del Pokémon
 }
