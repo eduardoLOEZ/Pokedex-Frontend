9334cc75a5ee10ba72856d310e86bfa6d0d851f0
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2024-05-28 09:05:47 -0600

    comentarios agregados

--- src/utils/generatePDF.ts
+++ src/utils/generatePDF.ts
@@ -1,10 +1,18 @@
 import jsPDF from "jspdf";
 import { DetailedPokemon } from "../interfaces/pokemon.interface";
 
-export const generatePDF = (pokemon: DetailedPokemon) => {
-  const doc = new jsPDF();
+/**
+ * Función para generar un archivo PDF con la información de un Pokémon detallado.
+ *
+ * @param pokemon - Objeto con los detalles del Pokémon
+ */
 
+export const generatePDF = (pokemon: DetailedPokemon) => {
+  const doc = new jsPDF(); // Crea una nueva instancia de jsPDF
+  // Establece el título del documento
   doc.setFontSize(20);
+
+  // Agrega detalles del Pokémon al documento
   doc.text(pokemon.name.toUpperCase(), 20, 20);
   doc.setFontSize(12);
   doc.text(`Base Experience: ${pokemon.base_experience}`, 20, 30);
@@ -15,14 +23,15 @@ export const generatePDF = (pokemon: DetailedPokemon) => {
     doc.text(`- ${ability.name}`, 20, 70 + index * 10);
   });
 
+  // Agregamos la imagen del Pokémon al documento si está disponible
   if (pokemon.image) {
     const img = new Image();
     img.src = pokemon.image;
     img.onload = () => {
       doc.addImage(img, "JPEG", 150, 20, 50, 50);
-      doc.save(`${pokemon.name}.pdf`);
+      doc.save(`${pokemon.name}.pdf`); // Guarda el documento como un archivo PDF
     };
   } else {
-    doc.save(`${pokemon.name}.pdf`);
+    doc.save(`${pokemon.name}.pdf`); // Guarda el documento como un archivo PDF sin imagen
   }
 };
